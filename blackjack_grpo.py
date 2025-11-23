fe0c3d7904772a1e3a6e708f1fe920a1816d749a
Nathan Jackson <nathan@csnate.com>
2025-11-23 15:05:37 -0500

    handle multiple turns correctly when constructing action mask

--- blackjack_grpo.py
+++ blackjack_grpo.py
@@ -19,31 +19,25 @@ class SimpleGRPOTrainer:
         self.model = transformers.AutoModelForCausalLM.from_pretrained(model_id, dtype=torch.bfloat16, device_map="auto")
 
     def generate_trajectory(self, game):
-        context = torch.empty(0, dtype=torch.long).to("cuda")
         action_mask = torch.empty(0, dtype=torch.long).to("cuda")
 
-        #system_text = self.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=False)
-        #print(system_text)
-        #system_text_ids = self.tokenizer(system_text, return_tensors="pt").to("cuda")
-        #context_tensors.append(system_text_ids.input_ids)
         messages = [
             { "role": "system", "content": "You are playing Blackjack. At each step, you'll be given the state of the game. Respond with HIT or STAY. Do not add commentary." },
         ]
 
+        invalid_action = False
         while not game.get_state()["game_over"]:
             visible_state = {
                 "your_hand": game.get_state()["player_hand"],
                 "dealer_hand": ["Hidden"] + game.get_state()["dealer_hand"][1:]
             }
             state_str = str(visible_state)
+            print(state_str)
             messages.append({ "role": "user", "content": state_str })
 
             messages_text = self.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
             messages_encoding = self.tokenizer(messages_text, return_tensors="pt").to("cuda")
-            context = torch.cat((context, messages_encoding["input_ids"]), dim=1)
             action_mask = torch.cat((action_mask, torch.zeros((1, len(messages_encoding["input_ids"][0])), dtype=torch.long).to("cuda")), dim=1)
-            print("context:", context)
-            print("action_mask:", action_mask)
 
             with torch.no_grad():
                 outputs = self.model.generate(
@@ -58,8 +52,6 @@ class SimpleGRPOTrainer:
 
             # Combine up response tokens and update the action mask.
             response_ids = outputs.sequences[0, messages_encoding.input_ids.shape[1]:]
-            context = torch.cat((context, response_ids.reshape((1, len(response_ids))) ), dim=1)
-            print(context)
 
             cur_action_mask = torch.ones(response_ids.shape, dtype=torch.long).to("cuda")
             cur_action_mask[response_ids == self.tokenizer.eos_token_id] = 0
@@ -75,51 +67,17 @@ class SimpleGRPOTrainer:
             elif "STAY" == action:
                 observation = game.stand()
             else:
-                raise ValueError("bad action")
+                invalid_action = True
+                break
 
-            print(observation)
+            # Update messages for the next iteration
+            messages.append({ "role": "assistant", "content": action })
 
-            raise NotImplementedError()
-            
-
-#            current_context = self.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
-#            #print(state_str)
-#            inputs = self.tokenizer(current_context, return_tensors="pt").to("cuda")
-#            print(inputs)
-#
-#            with torch.no_grad():
-#                outputs = self.model.generate(
-#                    **inputs,
-#                    max_new_tokens=32,
-#                    do_sample=True,
-#                    temperature=1.,
-#                    pad_token_id=self.tokenizer.pad_token_id,
-#                    return_dict_in_generate=True,
-#                )
-#
-#            new_ids = outputs.sequences[0, inputs.input_ids.shape[1]:]
-#
-#            response_text = self.tokenizer.decode(new_ids, skip_special_tokens=True)
-#            print(response_text)
-#
-#            messages.append({ "role": "assistant", "content": response_text })
-#
-#            if "HIT" == response_text:
-#                observation = game.hit()
-#            elif "STAY" == response_text:
-#                observation = game.stand()
-#            else:
-#                #print("invalid action")
-#                break
-
-            #print(observation)
-
-        #full_context = self.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=False)
-
-        return {
-            "game": game,
-            "full_context": messages,
-        }
+        # calculate reward for trajectory
+        trajectory = { "game": game, "messages": messages }
+        reward = self.compute_reward(trajectory)
+        print(reward)
+        return trajectory
 
     def compute_reward(self, trajectory):
         total_reward = 0.
@@ -207,9 +165,12 @@ def main():
     trainer = SimpleGRPOTrainer("ibm-granite/granite-4.0-350m")
     tokenizer = trainer.tokenizer
 
-    games = [blackjack.Blackjack() for _ in range(4)]
-    games[0].start_round()
-    trainer.generate_trajectory(games[0])
+    games = [blackjack.Blackjack() for _ in range(100)]
+
+    for g in games:
+        g.start_round()
+        trainer.generate_trajectory(g)
+        print()
 
 
 if "__main__" == __name__:
