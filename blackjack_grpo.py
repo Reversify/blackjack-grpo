7bf982105b911cfdb655ca85e23fac572a5ca1a8
Nathan Jackson <nathan@csnate.com>
2025-11-24 22:52:06 -0500

    fix trajectory and reward tensor sizes

--- blackjack_grpo.py
+++ blackjack_grpo.py
@@ -19,7 +19,7 @@ class SimpleGRPOTrainer:
         self.model = transformers.AutoModelForCausalLM.from_pretrained(model_id, dtype=torch.bfloat16, device_map="auto")
 
     def generate_trajectory(self, game):
-        action_mask = torch.empty(0, dtype=torch.long).to("cuda")
+        action_mask = None
 
         messages = [
             { "role": "system", "content": "You are playing Blackjack. At each step, you'll be given the state of the game. Respond with HIT or STAY. Do not add commentary." },
@@ -32,12 +32,11 @@ class SimpleGRPOTrainer:
                 "dealer_hand": ["Hidden"] + game.get_state()["dealer_hand"][1:]
             }
             state_str = str(visible_state)
-            print(state_str)
+            #print(state_str)
             messages.append({ "role": "user", "content": state_str })
 
             messages_text = self.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
             messages_encoding = self.tokenizer(messages_text, return_tensors="pt").to("cuda")
-            action_mask = torch.cat((action_mask, torch.zeros((1, len(messages_encoding["input_ids"][0])), dtype=torch.long).to("cuda")), dim=1)
 
             with torch.no_grad():
                 outputs = self.model.generate(
@@ -48,20 +47,25 @@ class SimpleGRPOTrainer:
                     pad_token_id=self.tokenizer.pad_token_id,
                     return_dict_in_generate=True,
                 )
-            print(outputs)
+            #print(outputs)
 
             # Combine up response tokens and update the action mask.
             response_ids = outputs.sequences[0, messages_encoding.input_ids.shape[1]:]
-
-            cur_action_mask = torch.ones(response_ids.shape, dtype=torch.long).to("cuda")
-            cur_action_mask[response_ids == self.tokenizer.eos_token_id] = 0
-            cur_action_mask = torch.reshape(cur_action_mask, (1, len(cur_action_mask)))
-            action_mask = torch.cat((action_mask, cur_action_mask), dim=1)
+            if action_mask is None:
+                action_mask = torch.zeros(outputs.sequences.shape[1])
+            else:
+                new_ids = outputs.sequences[0, action_mask.shape[0]:]
+                action_mask = torch.cat((action_mask, torch.zeros(new_ids.shape[0])), dim=0)
+            action_mask[messages_encoding.input_ids.shape[1]:][response_ids != self.tokenizer.eos_token_id] = 1
+            print(outputs.sequences)
             print(action_mask)
-
+            
             # decode and take the action, gather an observation
             action = self.tokenizer.decode(response_ids[:-1])
-            print(action)
+            # Update messages for the next iteration
+            messages.append({ "role": "assistant", "content": action })
+
+            #print(action)
             if "HIT" == action:
                 observation = game.hit()
             elif "STAY" == action:
@@ -70,37 +74,50 @@ class SimpleGRPOTrainer:
                 invalid_action = True
                 break
 
-            # Update messages for the next iteration
-            messages.append({ "role": "assistant", "content": action })
-
         # calculate reward for trajectory
         trajectory = { "game": game, "messages": messages }
         reward = self.compute_reward(trajectory)
-        print(reward)
+        reward_tensor = torch.zeros_like(action_mask, dtype=torch.float)
+        reward_tensor[torch.where(action_mask == 1)] = reward
+        trajectory["reward_tensor"] = reward_tensor
+
+        # compute logprobs for trajectory
+        trajectory_text = self.tokenizer.apply_chat_template(messages, add_generation_prompt=False, tokenize=False).strip()
+        print(trajectory_text)
+        trajectory_encoding = self.tokenizer(trajectory_text, return_tensors="pt").to("cuda")
+        print(trajectory_encoding.input_ids)
+        print(trajectory_encoding.input_ids.shape)
+        print(reward_tensor.shape)
+        assert trajectory_encoding.input_ids.shape[1] == reward_tensor.shape[0]
+
         return trajectory
 
     def compute_reward(self, trajectory):
         total_reward = 0.
         if not trajectory["game"].get_state()["game_over"]:
             # agent did not play properly
-            print("game did not finish")
+            #print("game did not finish")
             return -2.
         total_reward += 0.5
         outcome = trajectory["game"]._determine_winner()
         if "push" in outcome.lower():
             # tie
-            print("tie")
+            #print("tie")
             total_reward += 0.5
         elif "player wins" in outcome.lower():
             # agent win
-            print("agent win")
+            #print("agent win")
             total_reward += 2.
         else:
-            print("agent loss")
+            #print("agent loss")
             # agent loss
             return -1.
         return total_reward
 
+    def grpo_loss(self, trajectories):
+        reward_tensor = torch.stack([traj["reward_tensor"] for traj in trajectories], dim=0)
+        print(reward_tensor)
+        print(reward_tensor.shape)
 
     def train_step(self, games):
         self.model.train()
@@ -115,50 +132,12 @@ class SimpleGRPOTrainer:
             ]
             #print(trajectories)
 
-            # Score results
-            scores = [self.compute_reward(t) for t in trajectories]
-            rewards = [] 
-            for i, t in enumerate(trajectories):
-                context = t["full_context"]
-                tmp = torch.zeros(len(self.tokenizer.apply_chat_template(context, tokenize=True, padding="max_length", max_length=512)))
-                mask = action_mask(self.tokenizer, context)
-                tmp[mask == 1] = scores[i]
-                rewards.append(tmp)
-            rewards = torch.stack(rewards, dim=0)
-            #print(rewards)
-
-            
-            if len(set(rewards)) == 1:
+            #if len(set(rewards)) == 1:
                 # no signal for training since the scores were the same
-                continue
+            #    continue
 
             # compute loss
-
-def action_mask(tokenizer, context):
-    tokens = tokenizer.apply_chat_template(context, tokenize=True, padding="max_length", max_length=512)
-    target_words = ["HIT", "STAY"]
-    target_ids = tokenizer(target_words)["input_ids"]
-    action_indices = []
-    lengths = []
-    for targets in target_ids:
-        for i, _ in enumerate(tokens):
-            found = True
-            for j, tok2 in enumerate(targets):
-                if tok2 != tokens[i + j]:
-                    found = False
-                    break
-            if found:
-                action_indices.append(i)
-                lengths.append(len(targets))
-
-    mask = torch.zeros(len(tokens), dtype=torch.long)
-
-    for i, index in enumerate(action_indices):
-        run_len = lengths[i]
-        mask[index : index + run_len] = 1
-
-    return mask 
-
+            loss = self.grpo_loss(trajectories)
 
 
 def main():
@@ -167,10 +146,9 @@ def main():
 
     games = [blackjack.Blackjack() for _ in range(100)]
 
-    for g in games:
-        g.start_round()
-        trainer.generate_trajectory(g)
-        print()
+    #trainer.train_step(games)
+    games[0].start_round()
+    trainer.generate_trajectory(games[0])
 
 
 if "__main__" == __name__:
